6205b5b49b79b369dacf2ab644a9133f6cf6f110
Tatiana Kudiyarova <tatiana.kudiyarova@gmail.com>
2022-09-06 06:30:20 +1200

    update styling

--- src/components/ControlPane/ControlPane.js
+++ src/components/ControlPane/ControlPane.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import ToggleButton from '../ToggleButton';
-import ButtonRow from '../ButtonRow';
+import ToggleButton from "../ToggleButton";
+import ButtonRow from "../ButtonRow";
 
-import styles from './ControlPane.module.css';
+import styles from "./ControlPane.module.css";
 
 const ControlPane = ({
   title,
@@ -16,10 +16,7 @@ const ControlPane = ({
     <div className={styles.controlPane}>
       <h2 className={styles.title}>
         {title}
-
-        <span className={styles.metadata}>
-          {options.length} options
-        </span>
+        <span className={styles.metadata}>{options.length} options</span>
       </h2>
       <ButtonRow>
         {options.map(({ id, label, color, children }) => (
